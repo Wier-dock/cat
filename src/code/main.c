476c908c10d17d20b6d813130210c2298334c699
Wier-dock <sevajakson@gmail.com>
2025-02-11 09:22:40 +0300

    add flags -n and -b

--- src/code/main.c
+++ src/code/main.c
@@ -5,26 +5,26 @@ int main(int argc, char *argv[]) {
     perror("Введите название файла и флаги");
     return 1;
   } else {
-    struct Flags flags = {0,0,0,0,0,0};
+    struct Flags flags = {0, 0, 0, 0, 0, 0};
     if (argc > 1) {
       flag_parse(argv, &flags);
-      print_file(argc, argv);
-
-      printf("%d\n%d\n%d\n%d\n%d\n%d\n", flags.flagB, flags.flagE, flags.flagN,
-             flags.flagS, flags.flagT, flags.flagFree);
+      print_file(argc, argv, flags);
     }
   }
 
   return 0;
 }
 
-void print_file(int argc, char **argv, struct Flags *flags) {
+void print_file(int argc, char **argv, struct Flags flags) {
+  int startFromFile = 1;
   bool flag = false;
   if (argc - 1 > 1) {
     flag = true;
   }
-
-  for (int i = 1; i < argc; ++i) {
+  if (flags.flagFree == false) {
+    startFromFile = 2;
+  }
+  for (int i = startFromFile; i < argc; ++i) {
     FILE *file = fopen(argv[i], "r");
     if (file == NULL) {
       perror("Файла не существует");
@@ -35,9 +35,22 @@ void print_file(int argc, char **argv, struct Flags *flags) {
       printf("%s \n", argv[i]);
     }
 
+    int numberOfLine = 1;
     char buffer[1024];
     while (fgets(buffer, sizeof(buffer), file) != NULL) {
-      printf("%s", buffer);
+      if (flags.flagFree == true) {
+        printf("%s", buffer);
+      }
+      if (flags.flagN == true) {
+        printf("%d\t%s", numberOfLine++, buffer);
+      }
+      if (flags.flagB == true) {
+        if (strlen(buffer) >= 1 && buffer[0] != '\n') {
+          printf("%d\t%s", numberOfLine++, buffer);
+        } else {
+          printf("%s", buffer);
+        }
+      }
     }
 
     fclose(file);
