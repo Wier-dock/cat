07a527885c49374c2e4acdee4d09108caa60f8ad
Wier-dock <sevajakson@gmail.com>
2025-03-10 20:56:46 +0300

    edit main.c

--- src/code/main.c
+++ src/code/main.c
@@ -37,7 +37,8 @@ void print_file_EVT(FILE *fptr, struct Flags flags) {
     while (ch != EOF) {
       if (flags.flagE && ch == '\n') {
         printf("$");
-      }  if (flags.flagT && ch == '\t') {
+      }
+      if (flags.flagT && ch == '\t') {
         printf("^");
         ch = '\t' + 64;
       }
@@ -51,47 +52,38 @@ void print_file_EVT(FILE *fptr, struct Flags flags) {
 }
 
 void print_file_BNS(FILE *file, struct Flags flags) {
-  if(file != NULL){
+  if (file != NULL) {
     int ch = fgetc(file);
-    while(ch != EOF)
-  }
-    flags = true;
-  }
-  if (!flags.flagFree) {
-    startFromFile = 2;
-  }
-  for (int i = startFromFile; i < argc; ++i) {
-    FILE *file = fopen(argv[i], "r");
-    if (file == NULL) {
-      perror("Файла не существует");
-      return;
-    }
+    while (ch != EOF) }
+  flags = true;
+}
+if (!flags.flagFree) {
+  startFromFile = 2;
 
-    if (!flag) {
-      printf("%s \n", argv[i]);
-    }
+  if (!flag) {
+    printf("%s \n", argv[i]);
+  }
 
-    int numberOfLine = 1;
-    char buffer[1024];
-    while (fgets(buffer, sizeof(buffer), file) != NULL) {
-      if (!flags.flagFree) {
+  int numberOfLine = 1;
+  char buffer[1024];
+  while (fgets(buffer, sizeof(buffer), file) != NULL) {
+    if (!flags.flagFree) {
+      printf("%s", buffer);
+    }
+    if (!flags.flagN) {
+      printf("%d\t%s", numberOfLine++, buffer);
+    }
+    if (!flags.flagB) {
+      if (strlen(buffer) >= 1 && buffer[0] != '\n') {
+        printf("%d\t%s", numberOfLine++, buffer);
+      } else {
         printf("%s", buffer);
       }
-      if (!flags.flagN) {
-        printf("%d\t%s", numberOfLine++, buffer);
-      }
-      if (!flags.flagB) {
-        if (strlen(buffer) >= 1 && buffer[0] != '\n') {
-          printf("%d\t%s", numberOfLine++, buffer);
-        } else {
-          printf("%s", buffer);
-        }
-      }
     }
-
-    fclose(file);
   }
 
+  fclose(file);
+}
 
 void flag_parse(char **argv, struct Flags *flags) {
   if (!strcmp(argv[1], "-b")) {
